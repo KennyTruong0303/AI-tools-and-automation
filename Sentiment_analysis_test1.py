c388ca4fe9178686f118ff8c85bbb229edc8edfc
KennyTruong0303 <112433204+KennyTruong0303@users.noreply.github.com>
2023-04-27 16:33:17 +0200

    Delete Sentiment_analysis_test1.py

--- Sentiment_analysis_test1.py
+++
@@ -1,5 +0,0 @@
-from transformers import AutoTokenizer, AutoModelForSequenceClassification
-
-tokenizer = AutoTokenizer.from_pretrained("cardiffnlp/twitter-roberta-base-sentiment")
-
-model = AutoModelForSequenceClassification.from_pretrained("cardiffnlp/twitter-roberta-base-sentiment")
